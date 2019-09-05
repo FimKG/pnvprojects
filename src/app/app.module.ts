e348e315319e8b7ea52c1df28f68ee650ce8d8f3
FimKG <kgaugelob82@gmail.com>
2019-09-05 17:32:04 +0200

    nav-footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ServicesComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
