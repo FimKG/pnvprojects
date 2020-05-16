cc3ffffafa40a56c2370bbaf5e2d7815c620ed99
fimkg <kgaugelob82@gmail.com>
2020-05-16 11:29:47 +0200

    navbar + landing + scroll

--- js/custm.js
+++ js/custm.js
@@ -1,37 +1,80 @@
 $(document).ready(function () {
-	    // Add scrollspy to <body>
-	    $('body').scrollspy({ target: ".nav-scroll", offset: 50 });
-
-	    // Add smooth scrolling on all links inside the navbar
-	    $(".main-nav-collapse a").on('click', function (event) {
-
-	        // Prevent default anchor click behavior
-	        event.preventDefault();
-
-	        // Store hash
-	        var hash = this.hash;
-
-	        // Using jQuery's animate() method to add smooth page scroll
-	        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-	        $('html, body').animate({
-	            scrollTop: $(hash).offset().top
-	        }, 800, function () {
-
-	            // Add hash (#) to URL when done scrolling (default click behavior)
-	            window.location.hash = hash;
-	        });
-	    });
-
-
-	// function openNav() {
-	// 	document.getElementById("navbar").style.width = "250px";
-	// 	document.getElementById("main-nav").style.marginLeft = "250px";
-	// }
-
-	// function closeNav() {
-	// 	document.getElementById("navbar").style.width = "0";
-	// 	document.getElementById("main-nav").style.marginLeft = "0";
-	// }
-
+	"use strict";
 	$(".year").html(new Date().getFullYear());
+
+	//	var errors = false;
+	/* contact form validation */
+	$('form.contact_form').on('submit', function (event) {
+		event.preventDefault();
+		var this_type = $(this),
+			url = this_type.attr('action'),
+			type = this_type.attr('method'),
+			data = {};
+
+		this_type.find('[name]').each(function (index, value) {
+			var this_name = $(this),
+				name = this_name.attr('name'),
+				vale = this_name.val();
+
+			data[name] = vale;
+		});
+
+		// console.log(data)
+
+		$.ajax({
+			url: url,
+			type: type,
+			data: data,
+			success: function (response) {
+				console.log(response);
+				if (response == 'OK') {
+					console.log("data");
+					// this_form.find('.loading').slideUp();
+					// this_form.find('.sent-message').slideDown();
+					// this_form.find("input:not(input[type=submit]), textarea").val('');
+				} else {
+					console.log("error");
+					// this_form.find('.loading').slideUp();
+					// this_form.find('.error-message').slideDown().html(msg);
+				}
+			}
+		});
+		return false;
+	});
+
+	/* back-to-top */
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 220) {
+			$('#totop').fadeIn('slow');
+		} else {
+			$('#totop').fadeOut('slow');
+		}
+	});
+
+	$('#totop').click(function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1500, 'easeInOutExpo');
+		return false;
+	});
+
+	/* scrollto */
+
+	// Toggle .header-scrolled class to #header when page is scrolled
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+			$('#header').addClass('header-scrolled');
+			$('#top-header').addClass('top-header-scrolled');
+		} else {
+			$('#header').removeClass('header-scrolled');
+			$('#top-header').removeClass('top-header-scrolled');
+		}
+	});
+
+	if ($(window).scrollTop() > 50) {
+		$('#header').addClass('header-scrolled');
+		$('#top-header').addClass('top-header-scrolled');
+	}
+
+
 });
---
+++ php/contact.php
@@ -0,0 +1,24 @@
+<?php
+
+  $receiving_email_address = 'molotohouseplansa@gmail.com';
+  
+if (isset($_POST['name'], $_POST['subject'], $_POST['number'], $_POST['email'], $_POST['message'])) {
+
+    print_r($_POST);
+}
+
+// $contact = new PHP_Email_Form;
+// $contact->ajax = true;
+
+// $contact->to = $receiving_email_address;
+// $contact->from_name = $_POST['name'];
+// $contact->from_email = $_POST['email'];
+// $contact->subject = $_POST['subject'];
+// $contact->message = $_POST['message'] + $_POST['number'];
+
+// $contact->add_message( $_POST['name'], 'From');
+// $contact->add_message( $_POST['email'], 'Email');
+// $contact->add_message( $_POST['message'], 'Message', 10);
+
+// echo $contact->send();
+// 
